a5784cad6a5ca99e2089dff6fbf5f73705a6c7b9
Meghana_124 <meghanakankati24@gmail.com>
2026-04-15 09:40:04 +0530

    make poster to URLfield for deploying

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
@@ -133,3 +134,12 @@ MEDIA_ROOT = BASE_DIR / "media"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/accounts/login/"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
--- movies/models.py
+++ movies/models.py
@@ -8,7 +8,7 @@ class Movie(models.Model):
     language = models.CharField(max_length=50)
     release_date = models.DateField()
     description = models.TextField(blank=True)
-    poster = models.ImageField(upload_to="posters/", blank=True, null=True)
+    poster_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.title
